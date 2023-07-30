72179da09189afc11252bdb86b85c2bfc0023c18
Mariette Olwagen <90972322+MarietteO@users.noreply.github.com>
2023-07-31 00:17:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,46 @@
 from art import logo
 
-list_of_bidders = []
-list_of_bids = []
+bidding_dict = {}
 
-def append_lists():
-    """Asks user for names and bids. Adds these to list of bidders and list of bids."""
+
+def ask_info():
     name = input("What is your name? ")
-    bid = input("What is your bid? $")
-    if bid.isdigit():
-        list_of_bidders.append(name)
-        list_of_bids.append(bid)
-        return list_of_bidders, list_of_bids
+    bid = input("What is your bid? (Please enter a whole number, no cents): $ ")
+    test = bid.isdigit()
+    if test:
+        bid = int(bid)
+        bidding_dict[name] = bid
+        return bidding_dict
     else:
+        print("That answer is not valid. Please try again.")
         return False
 
 
-def find_max_bid():
-    """Finds the highest bid and name of the winner."""
-    max_bid = max(bids)
-    max_bid_index = bids.index(max_bid)
-    winner = bidders[max_bid_index]
-    print(f"The winner is {winner} with ${max_bid}.")
+def next_bidder():
+    while True:
+        cont = input("Is there another bidder? Type 'y' or 'n': ").lower()
+        if cont == "y":
+            return "y"
+        elif cont == "n":
+            return "n"
+        else:
+            print("That answer is not valid. Please try again.")
+
+
+def get_winner(foo):
+    return max(foo, key=foo.get)
+
+
+def get_winning_bid(foo):
+    return max(foo.values())
 
 
 print(logo)
 print("Welcome to the secret auction!")
-game_on = True
-while game_on:
-    appended_bids = append_lists()
-    if appended_bids:
-        bidders, bids = appended_bids
-        cont = input("Are there any other bidders? Type yes or no: ").lower()
-        if cont == "no":
-            find_max_bid()
-            game_on = False
-        elif cont == "yes":
-            pass
-        else:
-            print("That answer is not valid.")
-    else:
-        print("That answer is not valid.")
+
+game = True
+while game is True:
+    if ask_info():
+        if next_bidder() == "n":
+            break
+print(f"The winner is {get_winner(bidding_dict)}, with ${get_winning_bid(bidding_dict)}.")
